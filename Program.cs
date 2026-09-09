b168867d27dac6443a3341f9ca9da790b4f505ef
_Doaa_ <_Doaa_@DOAA>
2026-09-09 18:16:07 +0300

    q1

--- Program.cs
+++ Program.cs
@@ -1,10 +1,17 @@
-﻿namespace Session03_AssignmentC_
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace Session03_AssignmentC_
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Q1
+            string title = "clean code";
+            string upperTitle = title.ToUpper();
+            Console.WriteLine($"title: {title}");
+            Console.WriteLine($"upperTitle: {upperTitle}");
+            #endregion
         }
     }
 }

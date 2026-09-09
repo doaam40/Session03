d2edee62efa05520cf91e45b9c785fa4b434922d
_Doaa_ <_Doaa_@DOAA>
2026-09-09 18:22:00 +0300

    q2

--- Program.cs
+++ Program.cs
@@ -7,10 +7,16 @@ namespace Session03_AssignmentC_
         static void Main(string[] args)
         {
             #region Q1
-            string title = "clean code";
-            string upperTitle = title.ToUpper();
-            Console.WriteLine($"title: {title}");
-            Console.WriteLine($"upperTitle: {upperTitle}");
+            //string title = "clean code";
+            //string upperTitle = title.ToUpper();
+            //Console.WriteLine($"title: {title}");
+            //Console.WriteLine($"upperTitle: {upperTitle}");
+            #endregion
+
+            #region Q2
+            string string1 = "Clean Code";
+            string string2 = "Clean Code";
+            Console.WriteLine(object.ReferenceEquals(string1, string2));
             #endregion
         }
     }
